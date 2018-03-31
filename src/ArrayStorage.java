d5588780f1da7b30b8b02896bd12b4c37ed03129
ratanoff <leonid.ratanoff@gmail.com>
2018-03-31 14:40:13 +0500

    lesson 1 completed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,63 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int n = 0;
+        for (Resume aStorage : storage) {
+            if (aStorage != null) n++;
+        }
+
+        Resume[] result = new Resume[n];
+
+        int j = 0;
+        for (Resume aStorage : storage) {
+            if (aStorage != null) {
+                result[j] = aStorage;
+                j++;
+            }
+        }
+
+        return result;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
