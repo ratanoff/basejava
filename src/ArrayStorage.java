c07698e68ff80806f65189b851a1056a41da4e49
ratanoff <leonid.ratanoff@gmail.com>
2018-04-02 22:48:12 +0500

    HW1 Optional

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -12,8 +12,8 @@ public class ArrayStorage {
         size = 0;
     }
 
-    void save(Resume r) {
-        storage[size] = r;
+    void save(Resume resume) {
+        storage[size] = resume;
         size++;
     }
 
@@ -30,6 +30,7 @@ public class ArrayStorage {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
                 break;
             }
